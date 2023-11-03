d3a882e93d915370e43726880e20f9da8dcf8109
Adamrashid781 <86022836+Adamrashid781@users.noreply.github.com>
2023-11-03 03:25:42 -0700

    update for JavaScript project 4

--- Project3_math_operators/JS/main.js
+++ Project3_math_operators/JS/main.js
@@ -30,3 +30,38 @@ function many_math(){
     var math = (y*y) - (x / 7 + 100) ;
     document.getElementById('math').innerHTML = "(y * y) - x / 7 + 100 = " + math ;
 }
+
+function modulus_operator() {
+    var simple_math = 25 % 6 ; 
+    document.getElementById('modulus').innerHTML = "25 % 6 = " + simple_math ;
+}
+
+function negation_operator(){
+    var negation = 101 ;
+    document.getElementById('negation').innerHTML = - negation;
+}
+
+var inc_dec = 10 ;
+
+function increment(){
+    inc_dec++ ;
+    document.getElementById('increment').innerHTML = inc_dec ;
+}
+
+function decrement() {
+    inc_dec-- ;
+    document.getElementById('decrement').innerHTML = inc_dec ;
+}
+
+document.write("<br>", Math.random() * (11 - 10) + 10 ); 
+
+function my_Dictionary() {
+    var Animal = {
+        species: "Dog",
+        color: "Black",
+        breed: "Labrador",
+        age: 5,
+        sound: "Bark!"
+    } ;
+    document.getElementById("Dictionary").innerHTML = Animal.sound; 
+}
---
+++ Project4_dictionaries/JS/main.js
@@ -0,0 +1,14 @@
+
+
+function First_Dictionary() {
+    var cat = {
+        color: 'white',
+        hair: 'short' ,
+        sex: 'male',
+        age: '14 months'
+
+    };
+    delete cat.sex ; 
+    document.getElementById("Dictionary").innerHTML = 'My cat is ' + cat.sex;
+}
+
