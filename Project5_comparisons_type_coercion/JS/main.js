628475876f37fb7a4e7b64f4aca97bda254044ab
Adamrashid781 <86022836+Adamrashid781@users.noreply.github.com>
2023-11-03 20:32:18 -0700

    project 5 push

--- Project5_comparisons_type_coercion/JS/main.js
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -34,6 +34,54 @@ document.write("<br>" + (6 > 2)) ; // Will display true
 document.write("<br>" + (6 < 2)) ; // Will display false
 
 console.log(2 + 2) ;  // Will display "4" in the console of the browser page, use CTRL + SHFT + I 
-console.log(4 < 2);     // Will display false in the console of the browser page
+console.log(4 < 2) ;  // Will display false in the console of the browser page
 console.log(4 == (2 + 2));  // Will display true in the console of the browser page
 console.log(4 == (1 + 2)); // Will display false in the console of the browser page
+
+date1 = 10 ;
+date2 = 10 ;
+date3 = '10' ;
+date4 = 'ten' ; 
+
+// Triple equal sign This tells the computer to check 
+// whether the two pieces of data are equal in both VALUE and TYPE.
+document.write("<br>");
+document.write(date1 === date2) ; // TRUE
+document.write("<br>");
+document.write(date1 === date4) ; // FALSE
+document.write("<br>");
+document.write(date2 === date3) ; // FALSE
+document.write("<br>");
+document.write(date3 === date4) ; // FALSE
+document.write("<br>");
+
+
+// AND operator 
+document.write('AND operator') ; 
+document.write("<br>");
+document.write( 5 > 4 && 6 > 2); // TRUE
+document.write("<br>");
+document.write( 4 > 6 && 7 > 2); // FALSE
+document.write("<br>");
+
+// OR operator
+document.write('OR operator') ; 
+document.write("<br>");
+document.write( 5 > 6 || 6 > 2) // TRUE
+document.write("<br>");
+document.write( 5 > 8 || 1 > 2) // FALSE
+document.write("<br>");
+
+// NOT operator 
+document.write("NOT operator");
+document.write("<br>");
+document.write(20 != 19); // TRUE
+document.write("<br>");
+document.write( ! (20 > 21)); // TRUE
+document.write("<br>");
+document.write(20 != 20); // FALSE
+document.write("<br>");
+document.write( ! (20 < 21)); // TRUE
+document.write("<br>");
+
+
